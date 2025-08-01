7c9008156f24243997c61076428749c0a42fb7fd
Orzelius <albert.kostusev@siderolabs.com>
2025-08-01 20:13:41 +0900

    tmp (not signed)
    Signed-off-by: Orzelius <albert.kostusev@siderolabs.com>

---
+++ cmd.sh
@@ -0,0 +1,10 @@
+# talosctl cluster create/omni flags:
+--registry-mirror docker.io=http://10.5.0.1:5000 \
+--registry-mirror registry.k8s.io=http://10.5.0.1:5001  \
+--registry-mirror k8s.grc.io=http://10.5.0.1:5002 \
+--registry-mirror gcr.io=http://10.5.0.1:5003 \
+--registry-mirror ghcr.io=http://10.5.0.1:5004 \
+--registry-mirror 10.5.0.1:5005=http://10.5.0.1:5005 \
+--registry-mirror quay.io=http://10.5.0.1:5006 \
+--registry-mirror factory.talos.dev=http://10.5.0.1:5007 \
+
